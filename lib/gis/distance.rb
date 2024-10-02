ca62d1743f231d9a2883b7e0a1d6e1a3424f904d
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-10-02 06:18:08 -0400

    Just use the gem.

--- lib/gis/distance.rb
+++ lib/gis/distance.rb
@@ -8,7 +8,7 @@ module GIS
     class Error < StandardError; end
 
     # The version of the gis-distance library
-    VERSION = '1.1.0'
+    VERSION = '1.2.0'
 
     # Create a new GIS::Distance object using the two sets of coordinates
     # that are provided.
@@ -140,61 +140,8 @@ module GIS
 
     # See https://en.wikipedia.org/wiki/Vincenty's_formulae
     def vincenty_formula
-      require 'bigdecimal'
-      require 'bigdecimal/util'
-
-      # WGS-84 ellipsiod parameters
-      a = 6378137.0
-      f = 1 / 298.257223563
-      b = (1 - f) * a
-
-      # Convert degrees to radians
-      lat1 = @latitude1.to_d * Math::PI / 180
-      lon1 = @longitude1.to_d * Math::PI / 180
-      lat2 = @latitude2.to_d * Math::PI / 180
-      lon2 = @longitude2.to_d * Math::PI / 180
-
-      # Differences in coordinates
-      big_l = lon2 - lon1
-      u1 = Math.atan((1 - f) * Math.tan(lat1))
-      u2 = Math.atan((1 - f) * Math.tan(lat2))
-
-      sin_u1 = Math.sin(u1)
-      cos_u1 = Math.cos(u1)
-      sin_u2 = Math.sin(u2)
-      cos_u2 = Math.cos(u2)
-
-      λ = big_l
-      lambda_p = 2 * Math::PI
-      iterLimit = 100
-
-      while (λ - lambda_p).abs > 1e-12 && iterLimit > 0
-        sin_lambda = Math.sin(λ)
-        cos_lambda = Math.cos(λ)
-        sin_sigma = Math.sqrt((cos_u2 * sin_lambda) ** 2 + (cos_u1 * sin_u2 - sin_u1 * cos_u2 * cos_lambda) ** 2)
-        cos_sigma = sin_u1 * sin_u2 + cos_u1 * cos_u2 * cos_lambda
-        sigma = Math.atan2(sin_sigma, cos_sigma)
-        sin_alpha = cos_u1 * cos_u2 * sin_lambda / sin_sigma
-        cos2_alpha = 1 - sin_alpha ** 2
-        cos2_sigma_m = cos_sigma - 2 * sin_u1 * sin_u2 / cos2_alpha
-        big_c = f / 16 * cos2_alpha * (4 + f * (4 - 3 * cos2_alpha))
-        lambda_p = λ
-        λ = big_l + (1 - big_c) * f * sin_alpha * (sigma + big_c * sin_sigma * (cos2_sigma_m + big_c * cos_sigma * (-1 + 2 * cos2_sigma_m ** 2)))
-        iterLimit -= 1
-      end
-
-      if iterLimit == 0
-        return nil # formula failed to converge
-      end
-
-      u2 = cos2_alpha * (a ** 2 - b ** 2) / (b ** 2)
-      big_a = 1 + u2 / 16384 * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
-      big_b = u2 / 1024 * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
-      delta_sigma = big_b * sin_sigma * (cos2_sigma_m + big_b / 4 * (cos_sigma * (-1 + 2 * cos2_sigma_m ** 2) - big_b / 6 * cos2_sigma_m * (-3 + 4 * sin_sigma ** 2) * (-3 + 4 * cos2_sigma_m ** 2)))
-
-      s = b * big_a * (sigma - delta_sigma)
-
-      s.to_f / 1000.0
+      require 'rvincenty'
+      RVincenty.distance([@latitude1, @longitude1], [@latitude2, @longitude2]) / 1000.0
     end
 
     # Add a custom method to the base Float class if it isn't already defined.
